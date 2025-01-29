3404cc2f4e556c8831b8f7f93242c532c5076f68
Samridhi <samridhikhosla@gmail.com>
2025-01-28 20:19:34 -0700

    samridhi changes

---
+++ code/app/src/main/java/com/example/donotredeem/Rhombus.java
@@ -0,0 +1,11 @@
+package com.example.donotredeem;
+
+public class Rhombus extends Shape{
+    private int sides;
+    private String color;
+
+    public Rhombus(int sides, String color) {
+        this.sides = 4;
+        this.color = "Yellow";
+    }
+}
