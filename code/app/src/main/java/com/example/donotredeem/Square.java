4348f9a1c9bccb42e2ec4de2b6ae381df4671c59
aditya-gupta05 <88316131+aditya-gupta05@users.noreply.github.com>
2025-01-28 20:28:10 -0700

    First push

--- code/app/src/main/java/com/example/donotredeem/Square.java
+++
@@ -1,5 +0,0 @@
-package com.example.donotredeem;
-
-public class Square extends Shape{
-    private int side;
-}
--- code/app/src/main/java/com/example/donotredeem/Star.java
+++
@@ -1,6 +0,0 @@
-package com.example.donotredeem;
-
-public class Star extends Shape{
-    private int side;
-    
-}
