2cd1ceac4a8c0a1067500f1381949ddaee09a8f7
Ayaan Mallick <145244537+AyaanMallick@users.noreply.github.com>
2025-01-31 15:47:27 -0700

    Update Square.java

--- code/app/src/main/java/com/example/donotredeem/Square.java
+++ code/app/src/main/java/com/example/donotredeem/Square.java
@@ -2,4 +2,8 @@ package com.example.donotredeem;
 
 public class Square extends Shape{
     private int side;
+    private String color;
+    public Square (Integer side, String color){
+        this.color = "Green";
+        this.sides = 0;
 }
