bf3a6da73cb36d130c085a91080fb7ff8377d91e
Ayaan Mallick <mallick1@ualberta.ca>
2025-01-31 15:56:26 -0700

    Square

---
+++ code/app/src/main/java/com/example/donotredeem/Square.java
@@ -0,0 +1,11 @@
+package com.example.donotredeem;
+
+public class Square extends Shape {
+    private int side;
+    private String color;
+
+    public Square(Integer side, String color) {
+        this.color = "BLUE";
+        this.side = 0;
+    }
+}
