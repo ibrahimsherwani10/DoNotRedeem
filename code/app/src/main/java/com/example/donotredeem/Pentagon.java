343a79df2417a0d6eef8fb68452d5ce89d169d9c
aditya-gupta05 <88316131+aditya-gupta05@users.noreply.github.com>
2025-01-31 16:01:27 -0700

    Update Pentagon.java

--- code/app/src/main/java/com/example/donotredeem/Pentagon.java
+++ code/app/src/main/java/com/example/donotredeem/Pentagon.java
@@ -3,4 +3,9 @@ package com.example.donotredeem;
 public class Pentagon extends Shape{
     private int sides;
     private int diagonals;
+
+    public Pentagon(Integer side, String color){
+        this.color = "RED";
+        this.side = 0;
+    }
 }
